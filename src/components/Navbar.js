2091474c74b2e8c4b40915404dd5de6c6d81c58d
jalenlum <808jalen@gmail.com>
2023-11-15 08:52:21 -1000

    change to NavLink in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
 export const Navbar = () =>{
   return(
     <>
       <nav className="navbar navbar-expand-md navbar-custom">
         <div className="container">
-          <a href="" className="navbar-brand" style={{fontSize:30,fontWeight:600}}>Lift</a>
+          <NavLink to="/" className="navbar-brand" style={{fontSize:30,fontWeight:600}}>Lift</NavLink>
           <button className="navbar-toggler" data-bs-toggle="collapse"
           data-bs-target="#nav" aria-controls="nav" aria-label="Expand Navigation">
             <span className="navbar-toggler-icon"></span>
@@ -11,22 +14,19 @@ export const Navbar = () =>{
           <div className="collapse navbar-collapse" id="nav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a href="#" className="nav-link links">Home</a>
+                <NavLink to="/" className="nav-link links">Home</NavLink>
               </li>
               <li className="nav-item">
-                <a href="#" className="nav-link links">One Rep Max</a>
+                <NavLink to="/onerepmax" className="nav-link links">One Rep Max</NavLink>
               </li>
               <li className="nav-item">
-                <a href="#" className="nav-link links">Smolov</a>
+                <NavLink to="/smolov" className="nav-link links">Smolov</NavLink>
               </li>
               <li className="nav-item">
-                <a href="#" className="nav-link links">Smolov Jr.</a>
+                <NavLink to="/smolovjr" className="nav-link links">Smolov Jr.</NavLink>
               </li>
-              {/* <li className="nav-item">
-                <a href="#" className="nav-link links">Portfolio</a>
-              </li> */}
               <li className="nav-item">
-                <a href="#" className="nav-link links">Contact</a>
+                <NavLink to="/contact" className="nav-link links">Contact</NavLink>
               </li>
             </ul>
           </div>

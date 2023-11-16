8c2bdbf659c4fdefbea580f7c75a904f6d5fd9b8
jalenlum <808jalen@gmail.com>
2023-11-15 20:15:14 -1000

    smolov calculator functioning

--- src/components/SmolovJr.js
+++ src/components/SmolovJr.js
@@ -1,4 +1,18 @@
+import React, { useState } from "react"
+
 export const SmolovJr = () =>{
+  
+  const [maxLift, setMaxLift] = useState(0)
+  const [increment, setIncrement] = useState(5)
+  
+  const maxChange = (e) =>{
+    setMaxLift(e.target.value)
+  }
+
+  const incrementChange = (e) =>{
+    setIncrement(e.target.value)
+  }
+
   return(
     <>
       <div className="container">
@@ -7,17 +21,19 @@ export const SmolovJr = () =>{
             <h1>Smolov Jr.</h1>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <input type="number" />
-            <input type="number" />
+        <div className="row" style={{marginTop: 50}}>
+          <div className="col text-center">
+            <input type="number" value={maxLift} onChange={maxChange}/>
+          </div>
+          <div className="col text-center">
+            <input type="number" value={increment} onChange={incrementChange}/>
           </div>
         </div>
         <div className="row">
           <table class="table" style={{marginTop: 50}}>
             <thead>
               <tr>
-                <th scope="col">#</th>
+                <th scope="col">Day</th>
                 <th scope="col">Sets</th>
                 <th scope="col">Reps</th>
                 <th scope="col">Weight (lbs)</th>
@@ -25,28 +41,28 @@ export const SmolovJr = () =>{
             </thead>
             <tbody>
               <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <th scope="row">Monday</th>
+                <td>6</td>
+                <td>6</td>
+                <td>{maxLift * 0.7}</td>
               </tr>
               <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                <th scope="row">Wednesday</th>
+                <td>7</td>
+                <td>5</td>
+                <td>{maxLift * 0.75}</td>
               </tr>
               <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                <th scope="row">Friday</th>
+                <td>8</td>
+                <td>4</td>
+                <td>{maxLift * 0.8}</td>
               </tr>
               <tr>
-                <th scope="row">4</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                <th scope="row">Saturday</th>
+                <td>10</td>
+                <td>3</td>
+                <td>{maxLift * 0.85}</td>
               </tr>
             </tbody>
           </table>
@@ -54,7 +70,7 @@ export const SmolovJr = () =>{
           <table class="table" style={{marginTop: 50}}>
             <thead>
               <tr>
-                <th scope="col">#</th>
+                <th scope="col">Day</th>
                 <th scope="col">Sets</th>
                 <th scope="col">Reps</th>
                 <th scope="col">Weight (lbs)</th>
@@ -62,28 +78,28 @@ export const SmolovJr = () =>{
             </thead>
             <tbody>
               <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <th scope="row">Monday</th>
+                <td>6</td>
+                <td>6</td>
+                <td>{maxLift * 0.7 + parseInt(increment)}</td>
               </tr>
               <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                <th scope="row">Wednesday</th>
+                <td>7</td>
+                <td>5</td>
+                <td>{maxLift * 0.75 + parseInt(increment)}</td>
               </tr>
               <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                <th scope="row">Friday</th>
+                <td>8</td>
+                <td>4</td>
+                <td>{maxLift * 0.8 + parseInt(increment)}</td>
               </tr>
               <tr>
-                <th scope="row">4</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                <th scope="row">Saturday</th>
+                <td>10</td>
+                <td>3</td>
+                <td>{maxLift * 0.85 + parseInt(increment)}</td>
               </tr>
             </tbody>
           </table>
@@ -91,7 +107,7 @@ export const SmolovJr = () =>{
           <table class="table" style={{marginTop: 50}}>
             <thead>
               <tr>
-                <th scope="col">#</th>
+                <th scope="col">Day</th>
                 <th scope="col">Sets</th>
                 <th scope="col">Reps</th>
                 <th scope="col">Weight (lbs)</th>
@@ -99,28 +115,28 @@ export const SmolovJr = () =>{
             </thead>
             <tbody>
               <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <th scope="row">Monday</th>
+                <td>6</td>
+                <td>6</td>
+                <td>{maxLift * 0.7 + parseInt(increment) * 2}</td>
               </tr>
               <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                <th scope="row">Wednesday</th>
+                <td>7</td>
+                <td>5</td>
+                <td>{maxLift * 0.75 + parseInt(increment) * 2}</td>
               </tr>
               <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                <th scope="row">Friday</th>
+                <td>8</td>
+                <td>4</td>
+                <td>{maxLift * 0.8 + parseInt(increment) * 2}</td>
               </tr>
               <tr>
-                <th scope="row">4</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                <th scope="row">Saturday</th>
+                <td>10</td>
+                <td>3</td>
+                <td>{maxLift * 0.85 + parseInt(increment) * 2}</td>
               </tr>
             </tbody>
           </table>
